3bab10efaca72178c9aa207411a7a91a17dd48ef
Hoda ka <hodakakhki@Sinansoft.com>
2025-09-04 18:22:14 +0200

    Add navbar in the top

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
 import Hero from "@/components/Hero";
-import { FaHome } from "react-icons/fa";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
-        <FloatingNav
-          navItems={[]} // navItems={[name:'Home',link:'/'],icon:<FaHome />}
-        />
+        <FloatingNav navItems={navItems} />
         <Grid />
-        <RecentProjects/>
+        <RecentProjects />
       </div>
     </main>
   );
