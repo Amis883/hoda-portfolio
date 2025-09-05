a9ef2bcbfabd2fdb6e1bef653382e0e3c90bc152
Hoda ka <hodakakhki@Sinansoft.com>
2025-09-06 00:32:52 +0200

    feat: add Testimonials component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
+import Clients from "@/components/Clients";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
@@ -11,6 +12,7 @@ export default function Home() {
         <FloatingNav navItems={navItems} />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
