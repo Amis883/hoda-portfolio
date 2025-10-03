a61688a9206bf3dcc03c8b0839b7204796f31a73
Hoda ka <hodakakhki@Sinansoft.com>
2025-10-03 16:30:25 +0200

    chore: disable build-time ESLint/TS errors and relax rules

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+// };
+// module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true }, // ESLint را در build نادیده بگیر
+  typescript: { ignoreBuildErrors: true }, // TypeCheck ارور ندهد
 };
 module.exports = nextConfig;
