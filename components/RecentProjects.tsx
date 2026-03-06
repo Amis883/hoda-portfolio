4c2604817e3a1c0dca516da2b38ab9994236f402
Hoda ka <hodakakhki@Sinansoft.com>
2026-03-06 01:34:01 +0100

    add admin dashboard project to portfolio

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -20,11 +20,7 @@ function RecentProject() {
                 <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
-                <img
-                  src={img}
-                  alt={title}
-                  className="z-10 w-full h-full object-contain absolute"
-                />
+                <img src={img} alt={title} className="z-10 absolute bottom-0" />
               </div>
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-camp-1">
                 {title} 
